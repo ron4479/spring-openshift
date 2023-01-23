6bcc8bec36f3fe05753d5ca774779688e299d2f5
ronan <ronan.nival@gmail.coml>
2023-01-23 15:17:32 +0100

    test du reload auto

--- src/main/java/com/example/springopenshift/controller/HelloController.java
+++ src/main/java/com/example/springopenshift/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
     public class HelloController {
         @GetMapping("/hello-world")
         public String HelloWorld() {
-            return "Hello world test 2";
+            return "Hello world test reload auto";
         }
 
 }
